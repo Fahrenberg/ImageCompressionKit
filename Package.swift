cbf475851835e96f9fbbcf5d30113c439e9194e0
Jean-Nicolas <70505484+Fahrenberg@users.noreply.github.com>
2024-10-11 13:37:08 +0200

    Compile only for iOS (UIKit)

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "CompressorKit",
-    platforms: [.iOS(.v15), .macOS(.v11)],
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
