4d34cf05e2f2a371de496da7a1501068abb15d31
Jean-Nicolas <70505484+Fahrenberg@users.noreply.github.com>
2024-10-26 21:36:00 +0900

    feature: imageDataCompressing to compress data to a asked max byte size. change compression by half once.

--- Sources/ImageCompressionKit/ImageToDataCompression.swift
+++ Sources/ImageCompressionKit/ImageToDataCompression.swift
@@ -1,5 +1,6 @@
 import AVFoundation
 import OSLog
+
 // Color for macOS and iOS
 #if canImport(UIKit)
     import UIKit
@@ -10,27 +11,21 @@ import OSLog
 #endif
 
 extension PlatformImage {
-    enum HEICError: Error {
-        case heicNotSupported
-        case cgImageMissing
-        case canNotFinalize
-        case cgImageNotAddedAndFinished
-        case noImageCreated
-    }
     /// Use it only with [supported devices for HEIC](https://support.apple.com/en-us/HT207022)
     public func heicDataCompression(compressionQuality: CGFloat) -> Data? {
         let data = NSMutableData()
         let imageDestination =
-        CGImageDestinationCreateWithData(
-            data, AVFileType.heic as CFString, 1, nil
-        )
+            CGImageDestinationCreateWithData(
+                data, AVFileType.heic as CFString, 1, nil
+            )
         #if canImport(UIKit)
-        let cgImage = self.cgImage
+            let cgImage = self.cgImage
         #elseif canImport(AppKit)
-        let cgImage = self.cgImage(forProposedRect: nil, context: nil, hints: nil)
+            let cgImage = self.cgImage(
+                forProposedRect: nil, context: nil, hints: nil)
         #endif
         guard let cgImage,
-              let imageDestination
+            let imageDestination
         else {
             return nil
         }
@@ -51,8 +46,7 @@ extension PlatformImage {
 extension PlatformImage {
     /// Use it only with [supported devices for HEIC](https://support.apple.com/en-us/HT207022)
     #if canImport(UIKit)
-        public func jpgDataCompression(compressionQuality: CGFloat) -> Data?
-        {
+        public func jpgDataCompression(compressionQuality: CGFloat) -> Data? {
             return jpegData(compressionQuality: compressionQuality)
         }
     #endif
@@ -62,7 +56,10 @@ extension PlatformImage {
             let cgImage = image.cgImage(
                 forProposedRect: nil, context: nil, hints: nil)!
             let bitmapRep = NSBitmapImageRep(cgImage: cgImage)
-            let properties = [NSBitmapImageRep.PropertyKey.compressionFactor: compressionQuality]
+            let properties = [
+                NSBitmapImageRep.PropertyKey.compressionFactor:
+                    compressionQuality
+            ]
             let jpegData = bitmapRep.representation(
                 using: NSBitmapImageRep.FileType.jpeg, properties: properties)
             return jpegData
@@ -71,19 +68,58 @@ extension PlatformImage {
 }
 
 #if canImport(AppKit)
-extension NSImage {
-    func pngData() -> Data? {
-        tiffRepresentation?.bitmap?.png
+    extension NSImage {
+        func pngData() -> Data? {
+            tiffRepresentation?.bitmap?.png
+        }
+    }
+
+    extension NSBitmapImageRep {
+        var png: Data? { representation(using: .png, properties: [:]) }
+    }
+    extension Data {
+        var bitmap: NSBitmapImageRep? { NSBitmapImageRep(data: self) }
+    }
+#endif
+
+extension PlatformImage {
+    /// Compress NSImage or UIImage to data with proposed max size.
+    ///
+    /// Use heic compression algorythm if hardware (iPhone 6 or newer) allows.
+    /// Otherwise jpg compression.
+    /// If data size after compression is greater than proposedMaxSize, one more try with reduced compressionQuality.
+    ///
+    /// - returns: Compressed Image Data or nil (no compression possible)
+    func imageDataCompressing(
+        compressionQuality: CGFloat,
+        askedMaxBytes: UInt64 = .max
+    ) -> Data? {
+        if let heicData = self.heicDataCompression(
+            compressionQuality: compressionQuality)
+        {
+            guard heicData.count > askedMaxBytes else { return heicData }
+            // one more try with half compressionQuality
+            guard
+                let minimumHeicData = self.heicDataCompression(
+                    compressionQuality: compressionQuality / 2.0)
+            else { return heicData }
+            return minimumHeicData
+        } else {
+            if let jpgData = self.jpgDataCompression(
+                compressionQuality: compressionQuality)
+            {
+                guard jpgData.count > askedMaxBytes else { return jpgData }
+                // one more try with half compressionQuality
+                guard
+                    let minimumJpgData = self.jpgDataCompression(
+                        compressionQuality: compressionQuality / 2.0)
+                else { return jpgData }
+                return minimumJpgData
+            }
+        }
+        return nil  // no compression possible
     }
 }
-
-extension NSBitmapImageRep {
-    var png: Data? { representation(using: .png, properties: [:]) }
-}
-extension Data {
-    var bitmap: NSBitmapImageRep? { NSBitmapImageRep(data: self) }
-}
-#endif
 
 extension Logger {
     static let subsystem = "\(Bundle.main.bundleIdentifier!)"
