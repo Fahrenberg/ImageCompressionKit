9b8c1fb197c435eaf269c213773994e2ebd18391
Jean-Nicolas <70505484+Fahrenberg@users.noreply.github.com>
2025-02-25 21:26:15 +0100

    tests: ImageResizeTests added frame and fill to resized image

--- Tests/ImageCompressionKitTests/ImageResizeTests.swift
+++ Tests/ImageCompressionKitTests/ImageResizeTests.swift
@@ -12,39 +12,54 @@ import Extensions
 @testable import ImageCompressionKit
 
 final class ImageResizeTests: XCTestCase {
+    
+    override func setUpWithError() throws {
+        let tmpDir = try PlatformImage.tempDirectory()
+        FileManager.deleteAllFiles(directoryURL: tmpDir)  // reset
+    }
+    
     func testResizeMediumImage() throws {
         let image = try XCTUnwrap(TestImage.image(size: .medium))
         Logger.test.info("image size: \(image.sizeDescription, privacy: .public)")
         let expectedHeight = 500
         let expectedWidth = 500
+        
+        // ** function resized to test **
         let resizedImage = image.resized(to: CGSize(width: expectedWidth, height: expectedHeight))
+        // **
+        
         let resultWidth = Int(try XCTUnwrap(resizedImage?.size.width))
         let resultHeight = Int(try XCTUnwrap(resizedImage?.size.height))
         Logger.test.info("image resized: \(resizedImage?.sizeDescription ?? "nil", privacy: .public)")
         XCTAssertEqual(resultHeight, expectedHeight)
         XCTAssertEqual(resultWidth, expectedWidth)
         
+        let resizedImageUnWrapped = try XCTUnwrap(resizedImage)
+        let framedImage = resizedImageUnWrapped.addFrame().fillFrame() // for better visibility fill and frame image
         // write resizedImage to disk for preview
-        try resizedImage?.writeToDisk(filename: "resizedImage.bmp")
+        try framedImage.writeToDisk(filename: "testResizeMediumImage.bmp")
     }
     
     // alignment (default = center)
     // fill transparent pixels (optional)
-    
-    func testResizedImageCenterAlignment() throws {
+    func testAlignmentForResizedImage() throws {
         let image = try XCTUnwrap(TestImage.image(size: .small))
         Logger.test.info("image size: \(image.sizeDescription, privacy: .public)")
-
+        
         let imageCenterAligned = try XCTUnwrap(TestImage.image(size: .small_center))
         Logger.test.info("imageCenterAligned size: \(imageCenterAligned.sizeDescription, privacy: .public)")
         
         let expectedHeight = Int(imageCenterAligned.size.height)
         let expectedWidth = Int(imageCenterAligned.size.width)
         Logger.test.info("expectedHeight: \(expectedHeight, privacy: .public), expectedWidth: \(expectedWidth)")
+        
+        // ** function resized to test **
         let resizedImage = image.resized(
             to: CGSize(width: expectedWidth, height: expectedHeight),
             alignment: .center
         )
+        // **
+
         let resultWidth = Int(try XCTUnwrap(resizedImage?.size.width))
         let resultHeight = Int(try XCTUnwrap(resizedImage?.size.height))
         Logger.test.info("resizedImage: \(resizedImage?.sizeDescription ?? "nil", privacy: .public)")
@@ -53,9 +68,11 @@ final class ImageResizeTests: XCTestCase {
         
         // Alignment test: images size must be equal
         XCTAssertEqual(resizedImage?.pngData()?.count, imageCenterAligned.pngData()?.count)
+        
+        let resizedImageVisible = try XCTUnwrap(resizedImage)
+        let savedResizedImage = resizedImageVisible.addFrame().fillFrame() // for better visibility
         // write resizedImage to disk for preview
-        try resizedImage?.writeToDisk(filename: "resizedAlignedImage.bmp")
+        try savedResizedImage.writeToDisk(filename: "testAlignmentForResizedImage.bmp")
         
     }
-
 }
